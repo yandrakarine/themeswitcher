c33dfde5dccc03399f37fdd7f783a4c5c4bfbb07
yandrakarine <yandrakarine.dev@outlook.com>
2023-03-27 14:24:44 -0300

    project init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Hello, World</h1>
+    </div>
+  )
+}
+
+export default App
