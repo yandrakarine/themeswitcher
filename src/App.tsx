756de13ef05130e2c13da0234f1cec9eb3c25821
yandrakarine <yandrakarine.dev@outlook.com>
2023-03-27 18:07:27 -0300

    add custom Hook usePersistedState

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useCallback, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import Header from "./components/Header"
 import GlobalStyle from "./styles/GlobalStyle"
 import light from './styles/themes/light'
 import dark from './styles/themes/dark'
+import usePersistedState from './utils/usePersistedState'
+import { DefaultTheme } from 'styled-components'
 
 const App = () => {
-  const [theme, setTheme] = useState(light)
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light)
 
   const toggleTheme = () => {
-    setTheme(prev => prev.title === 'light' ? dark : light )
+    setTheme((prev: { title: string })  => prev.title === 'light' ? dark : light )
   }
 
     return(
