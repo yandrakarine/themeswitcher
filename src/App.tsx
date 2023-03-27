72e87436950c24f8620cc8d7cc740817f19aebd2
yandrakarine <yandrakarine.dev@outlook.com>
2023-03-27 17:05:11 -0300

    create state for ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+import { useCallback, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import Header from "./components/Header"
 import GlobalStyle from "./styles/GlobalStyle"
 import light from './styles/themes/light'
+import dark from './styles/themes/dark'
 
-const App = () => (
- 
-    <ThemeProvider theme={light}>
+const App = () => {
+  const [theme, setTheme] = useState(light)
+
+  const toggleTheme = () => {
+    setTheme(prev => prev.title === 'light' ? dark : light )
+  }
+
+    return(
+      <ThemeProvider theme={theme}>
       <div className="App">
         <GlobalStyle/>
-        <Header/>
+        <Header toggleTheme={toggleTheme}/>
       </div>
     </ThemeProvider>
-
-)
+    )
+  }
 
 export default App
